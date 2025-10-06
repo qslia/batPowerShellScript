00ed82cf98223a53267ce454da697cf02cf643bb
qslia <qsliang456@163.com>
2025-10-06 09:19:20 +0800

    organize folder

---
+++ bat/steam.bat
@@ -0,0 +1,9 @@
+@echo off
+chcp 65001 >nul
+echo === Launching UU加速器 and Steam ===
+
+start "" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\UU加速器.lnk"
+start "" "C:\Users\qslia\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Steam\Steam.lnk"
+start "" "C:\Program Files\365VPN\365VPN.exe"
+start "" "C:\Users\qslia\Desktop\run_script.bat"
+echo === Done! ===
